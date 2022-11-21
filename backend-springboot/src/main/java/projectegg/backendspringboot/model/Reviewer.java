ecf9f66deb049b9b905c61c20a7e8e2f7edfb81c
kgbigoness <kgbigoness@gmail.com>
2022-11-21 18:37:40 -0500

    initial commits

---
+++ backend-springboot/src/main/java/projectegg/backendspringboot/model/Reviewer.java
@@ -0,0 +1,72 @@
+package projectegg.backendspringboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Reviewer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "email_id")
+    private String emailId;
+    
+    public Reviewer() {
+
+    }
+
+    public Reviewer(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    } 
+
+    
+
+
+}
---
+++ react-frontend/src/components/HomeComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class HomeComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default HomeComponent;
