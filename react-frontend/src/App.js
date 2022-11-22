6ae2f8fb7b826382b17ad1df0ab1b73a94893d38
kgbigoness <kgbigoness@gmail.com>
2022-11-21 19:05:05 -0500

    react up and running hello world

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeComponent from './components/HomeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div>
+          <Switch>
+            <Route path="/" exact component={HomeComponent}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- react-frontend/src/components/HomeComponent.jsx
+++ react-frontend/src/components/HomeComponent.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class HomeComponent extends Component {
   constructor(props) {
